ae4c35b092f1d5fc9efa3c22de0e150f7b96b211
Renato Lui Geh <renatogeh@gmail.com>
2015-11-17 17:56:40 -0200

    Adicionar Recipes em User.

--- BudgetChef-Core/src/budgetchef/User.java
+++ BudgetChef-Core/src/budgetchef/User.java
@@ -12,4 +12,23 @@ public class User {
 
   public String getName() { return name_; }
   public Ingredient[] getIngredients() { return fridge_.getIngredients(); }
+  
+  public Recipe getRecipe(String name) {
+    for (Recipe r : recipes_)
+      if (r.getName().equals(name))
+        return r;
+    return null;
+  }
+  
+  public String[] getRecipes() {
+    int recipesSize = recipes_.size();
+    String[] array = new String[recipesSize];
+    
+    for (int i = 0; i < recipesSize; ++i)
+      array[i] = recipes_.get(i).getName();
+    
+    return array;
+  }
+  
+  public void addRecipe(Recipe r) { recipes_.add(r); }
 }
